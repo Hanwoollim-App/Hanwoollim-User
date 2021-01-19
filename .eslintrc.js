d748dc90796687e0a5111f1ca153f3254ef7c2bd
동현 김 <akainoo@naver.com>
2021-01-19 16:27:46 +0900

    feat : eslint & prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: 'babel-eslint',
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'@react-native-community',
+		'naver',
+	],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'import/prefer-default-export': 'off',
+		'no-mixed-spaces-and-tabs': 'off',
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': ['error'],
+		'linebreak-style': 0,
+		// production 환경에서는 on 으로 되어있어야 함.
+		'no-console': 'off',
+		'arrow-parens': ['error', 'always'],
+	},
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
+  useTabs: true,
   singleQuote: true,
   trailingComma: 'all',
 };
