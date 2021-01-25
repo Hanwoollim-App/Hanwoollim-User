bcbcdd814886d6f22e2a57481b3d6223b5f0c516
동현 김 <akainoo@naver.com>
2021-01-25 16:23:24 +0900

    feat : main 화면에선 바로 앱 종료 구현

--- app/components/screens/Home.tsx
+++ app/components/screens/Home.tsx
@@ -1,7 +1,20 @@
-import React from "react";
-import {View, Text} from "react-native";
+import React, {useEffect} from "react";
+import {View, Text, BackHandler} from "react-native";
 
 function Home() {
+	useEffect(() => {
+		const backAction = () => {
+			BackHandler.exitApp();
+		};
+
+		const backHandler = BackHandler.addEventListener(
+			"hardwareBackPress",
+			backAction,
+		);
+
+		return () => backHandler.remove();
+	}, []);
+
 	return (
 		<View>
 			<Text>{"홈 화면입니다."}</Text>
