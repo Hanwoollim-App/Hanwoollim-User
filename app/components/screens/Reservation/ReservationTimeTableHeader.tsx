36eee54fafb888dba3b091bf2514b19c7090068b
동현 김 <akainoo@naver.com>
2021-02-06 15:56:05 +0900

    feat : HeaderBar에 Picker Select 구현

--- app/components/screens/Reservation/ReservationTimeTableHeader.tsx
+++ app/components/screens/Reservation/ReservationTimeTableHeader.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import {TouchableOpacity, View, Text, StyleSheet} from "react-native";
+import RNPickerSelect from "react-native-picker-select";
+
+// 라이브러리
+const pickerSelectStyles = StyleSheet.create({
+	inputIOS: {
+		width: 138,
+		height: 26,
+		backgroundColor: "#ffffff",
+		borderStyle: "solid",
+		borderWidth: 1,
+		borderColor: "#00203f",
+		textAlign: "center",
+		fontFamily: "KoreanYNSJG3",
+		fontSize: 12,
+	},
+	inputAndroid: {
+		width: "100%",
+		height: "100%",
+		backgroundColor: "#ffffff",
+		borderStyle: "solid",
+		borderWidth: 1,
+		borderColor: "#00203f",
+		textAlign: "center",
+		fontFamily: "KoreanYNSJG3",
+		fontSize: 12,
+	},
+});
 
 const styles = StyleSheet.create({
 	rootView: {
@@ -8,6 +35,15 @@ const styles = StyleSheet.create({
 		backgroundColor: "#ffffff",
 		flexDirection: "row",
 		alignItems: "center", // header 안에 있는 Component들은 수직 정렬되어 있음
+	},
+	picker:{
+		width: 138,
+		height: 26,
+		marginLeft: 28,
+		backgroundColor: "#ffffff",
+		borderStyle: "solid",
+		borderWidth: 1,
+		borderColor: "#00203f",
 	},
 	reserveBtnView: {
 		marginLeft: 100,
@@ -36,9 +72,26 @@ const styles = StyleSheet.create({
 	},
 });
 
+interface pickerValueInteface {
+	label: string, // "1"->이번주, "2"->다음주
+	value: string,
+}
+
 function ReservationTimeTableHeader({btnListener}) {
+	const [pickerValue, setPickerValue] : [pickerValueInteface, Function] = useState({label: "2021. 02. 1주차  ▽", value: "1"});
+
+
 	return (
 		<View style={styles.rootView}>
+			<View style={styles.picker}>
+				<RNPickerSelect
+					style={pickerSelectStyles}
+					onValueChange={(value) => setPickerValue(value)}
+					placeholder={{}}
+					value={pickerValue.value}
+					items={[{label: "2021. 02. 1주차  ▽", value: "1"}, {label: "2021. 02. 2주차  ▽", value: "2"}]}
+				/>
+			</View>
 			<View style={styles.reserveBtnView}>
 				<TouchableOpacity
 					style={styles.reserveBtn}
