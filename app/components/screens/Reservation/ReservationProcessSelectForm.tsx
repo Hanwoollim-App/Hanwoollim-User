50a8d134a56a3a8f988fb1a5ccb5ddee3227b87f
동현 김 <akainoo@naver.com>
2021-02-11 21:40:07 +0900

    feat : 기본적인 UI & UX 구조 구현

--- app/components/screens/Reservation/ReservationProcessSelectForm.tsx
+++ app/components/screens/Reservation/ReservationProcessSelectForm.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
+import React from "react";
 import {View, Text, StyleSheet} from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 
-const styles= StyleSheet.create({
+interface pickerPropsInterface {
+	placeholder: Object,
+	style: Object,
+	items: Array<any>,
+	value: any,
+	onValueChange: Function,
+}
 
-})
+interface SelectFormPropsInterface{
+	title : string,
+	pickerProps: pickerPropsInterface,
+}
 
-function ReservationProcessSelectForm({title, pickerProps}) {
-    return (
-        <View style={styles.reservationUnit}>
-					<Text>{`예약 단위`}</Text>
-					<View>
-						<RNPickerSelect
-							placeholder={{}}
-							style={pickerStyle}
-							items={dayItems}
-							value={dayItems[0]}
-							onValueChange={() => console.log(dayItems)}
-						/>
-					</View>
-        </View>
-    );
+const styles = StyleSheet.create({
+	rootView: {
+		flexDirection: "row",
+		marginTop: 32,
+	},
+	title: {
+		width: 53,
+		height: "50%",
+		marginLeft: 40,
+		fontFamily: "KoreanYNSJG4",
+		fontSize: 13,
+		fontWeight: "normal",
+		fontStyle: "normal",
+		lineHeight: 18,
+		letterSpacing: 0,
+		textAlign: "left",
+		color: "#363636",
+	},
+	pickerView: {
+		width: 113,
+		height: "100%",
+		marginLeft: 96,
+	},
+});
+
+function ReservationProcessSelectForm({title, pickerProps} : SelectFormPropsInterface) {
+	return (
+		<View style={styles.rootView}>
+			<Text style={styles.title}>
+				{`${title}`}
+			</Text>
+			<View style={styles.pickerView}>
+				<RNPickerSelect
+					placeholder={pickerProps.placeholder}
+					style={pickerProps.style}
+					items={pickerProps.items}
+					value={pickerProps.value}
+					onValueChange={(value) => {
+						pickerProps.onValueChange(value);
+					}}
+				/>
+			</View>
+		</View>
+	);
 }
 
 export default ReservationProcessSelectForm;
--- app/components/screens/Reservation/ReservingProcess.tsx
+++ app/components/screens/Reservation/ReservingProcess.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {View, StyleSheet, Text} from "react-native";
 import RNPickerSelect from "react-native-picker-select";
+import {useNavigation} from "@react-navigation/native";
 import CustomBtn from "../../common/CustomBtn";
 import ReservationProcessHeader from "./ReservationProcessHeader";
 import ReservationProcessSelectForm from "./ReservationProcessSelectForm";
+import color from "../../../utils/constant/common/design/Color";
 
 const pickerStyle = StyleSheet.create({
 	inputIOS: {
@@ -21,7 +23,7 @@ const styles = StyleSheet.create({
 	rootView: {
 		flex: 1,
 	},
-	picker: {
+	dayPicker: {
 		width: 113,
 		height: 26,
 		marginLeft: 25,
@@ -39,57 +41,183 @@ const styles = StyleSheet.create({
 		borderWidth: 1,
 		borderColor: "#bdbdbd",
 	},
-	reservationForm: {
-
+	reservationDefaultInfo: {
+		width: 327,
+		height: 151,
+		marginTop: 15,
+		borderRadius: 11,
+		backgroundColor: "#ffffff",
+		borderStyle: "solid",
+		borderWidth: 1,
+		borderColor: "#bdbdbd",
 	},
-})
+	reservationSectionInfo: {
+		width: 327,
+		height: 118,
+		marginTop: 13,
+		borderRadius: 11,
+		backgroundColor: "#ffffff",
+		borderStyle: "solid",
+		borderWidth: 1,
+		borderColor: "#bdbdbd",
+	},
+	alertText: {
+		width: 164,
+		height: 8,
+		marginTop: 8,
+		fontFamily: "KoreanYNSJG2",
+		fontSize: 8,
+		fontWeight: "normal",
+		fontStyle: "normal",
+		lineHeight: 11,
+		letterSpacing: 0,
+		textAlign: "left",
+		color: "#363636",
+	},
+	addBtn: {
+		width: 130,
+		height: 12,
+		marginTop: 23,
+	},
+	addBtnText: {
+		width: "100%",
+		height: "100%",
+		fontFamily: "KoreanYNSJG4",
+		fontSize: 12,
+		fontWeight: "normal",
+		fontStyle: "normal",
+		lineHeight: 16,
+		letterSpacing: 0,
+		textAlign: "left",
+		color: color.mainColor,
+	},
+	submitView: {
+		width: 290,
+		height: 42,
+		marginTop: 265,
+		borderRadius: 21,
+		backgroundColor: color.mainColor,
+	},
+	submitViewBtn: {
+		width: "100%",
+		height: "100%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	sumbitViewText: {
+		width: 78,
+		height: 13,
+		fontFamily: "KoreanYNSJG4",
+		fontSize: 13,
+		fontWeight: "normal",
+		fontStyle: "normal",
+		lineHeight: 18,
+		letterSpacing: 0,
+		textAlign: "center",
+		color: "#ffffff",
+	},
+});
 
 const dayItems = [{label: "월요일", value: "1"}, {label: "화요일", value: "2"}, {label: "수요일", value: "3"}, {label: "목요일", value: "4"}, {label: "금요일", value: "5"}, {label: "토요일", value: "6"}, {label: "일요일", value: "7"}];
+const unitItems = [{label: "개인", value: "1"}, {label: "팀", value: "2"}];
+const sectionItems = [{label: "드럼", value: "1"}, {label: "기타", value: "2"}, {label: "베이스", value: "3"}, {label: "건반", value: "4"}, {label: "보컬", value: "5"}];
+const timeItems = [];
+
+for (let i = 0; i < 24; i++) {
+	timeItems.push({label: `${i}시`, value: `${i + 1}`});
+}
 
 function ReservingProcess({route}) {
-	const {currentWeek} : any = route.params; // ts 형식으로 바꿀 필요 있음
-
+	const navigation = useNavigation();
+	const {currentWeek}: any = route.params; // ts 형식으로 바꿀 필요 있음
+	const onUnitChangeListener = (value) => {
+		console.log(value);
+	};
+	const onTimeChangeListener = (value) => {
+		console.log(value);
+	};
+	const onSectionChangeListener = (value) => {
+		console.log(value);
+	};
+	const onSectionAddBtnClickListener = () => {
+		console.log("추가!");
+	};
+	const onsumbitBtnClickListener = () => {
+		navigation.navigate("BottomTabNavigator", {
+			screen: "Home",
+		});
+	};
 
 	return (
 		<View style={styles.rootView}>
 			<ReservationProcessHeader
 				currentWeek={currentWeek}
 			/>
-			<View style={styles.picker}>
+			<View style={styles.dayPicker}>
 				<RNPickerSelect
 					placeholder={{}}
 					style={pickerStyle}
 					items={dayItems}
 					value={dayItems[0]}
-					onValueChange={() => console.log(dayItems)}
+					onValueChange={(value) => console.log(value)}
 				/>
 			</View>
 			<View style={styles.timeBox}>
 				<Text> {`시간들이 들어갈 공간`}</Text>
 			</View>
 			<View style={styles.reservationDefaultInfo}>
-				<ReservationProcessSelectForm/>
-				<ReservationProcessSelectForm/>
-				<Text>{`하루에 개인당 최대 한시간만 예약이 가능합니다`}</Text>
+				<ReservationProcessSelectForm
+					title={`예약 단위`}
+					pickerProps={{
+						placeholder: {},
+						style: pickerStyle,
+						items: unitItems,
+						value: unitItems[0],
+						onValueChange: onUnitChangeListener,
+					}}
+				/>
+				<ReservationProcessSelectForm
+					title={`예약 시간`}
+					pickerProps={{
+						placeholder: {},
+						style: pickerStyle,
+						items: timeItems,
+						value: timeItems[0],
+						onValueChange: onTimeChangeListener,
+					}}
+				/>
+				<Text style={styles.alertText}>
+					{`하루에 개인당 최대 한시간만 예약이 가능합니다`}
+				</Text>
 			</View>
-			<View>
+			<View style={styles.reservationSectionInfo}>
 				<View>
-					<ReservationProcessSelectForm/>
+					<ReservationProcessSelectForm
+						title={`세션 1`}
+						pickerProps={{
+							placeholder: {},
+							style: pickerStyle,
+							items: sectionItems,
+							value: sectionItems[0],
+							onValueChange: onSectionChangeListener,
+						}}
+					/>
 				</View>
 				<CustomBtn
-					title={}
-					onClickListener={}
-					titleStyle={}
-					btnStyle={}
+					title={`세션 추가하기 (최대 2개)`}
+					onClickListener={onSectionAddBtnClickListener}
+					btnStyle={styles.addBtn}
+					titleStyle={styles.addBtnText}
 				/>
 			</View>
-
-			<CustomBtn
-				title={}
-				onClickListener={}
-				titleStyle={}
-				btnStyle={}
-			/>
+			<View style={styles.submitView}>
+				<CustomBtn
+					title={`예약 확정하기`}
+					onClickListener={onsumbitBtnClickListener}
+					titleStyle={styles.sumbitViewText}
+					btnStyle={styles.submitViewBtn}
+				/>
+			</View>
 		</View>
 	);
 }
