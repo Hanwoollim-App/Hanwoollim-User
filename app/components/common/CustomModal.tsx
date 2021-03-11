b34fb1378c05cfcccae7f5a4754267fac7c7599a
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-03-11 17:16:43 +0900

    feat: CustomModal 반응형 작업

--- app/components/common/CustomModal.tsx
+++ app/components/common/CustomModal.tsx
@@ -1,77 +1,74 @@
 import React from "react";
 import {Text, View, Modal, StyleSheet} from "react-native";
-import {RFValue} from "react-native-responsive-fontsize";
 import color from "../../utils/constant/common/design/Color";
 import CustomBtn from "./CustomBtn";
+import {fontPercentage, heightPercentage, widthPercentage} from "../../utils/constant/common/design/Responsive";
 
 const styles = StyleSheet.create({
 	modalView: {
+		width: widthPercentage(250),
+		height: heightPercentage(159),
 		justifyContent: "space-between",
 		alignItems: "center",
-		width: "66%",
-		height: "20%",
-		marginTop: "80%",
-		marginLeft: "17%",
+		marginTop: heightPercentage(327),
+		marginLeft: widthPercentage(63),
 		backgroundColor: "white",
 		elevation: 5,
 	},
 	contentContainer: {
-		flex: 7.2,
+		height: heightPercentage(115),
 		justifyContent: "center",
 		alignItems: "center",
-		width: "100%",
 		backgroundColor: "white",
 	},
 	btnContainer: {
-		flex: 3.8,
 		flexDirection: "row",
 		justifyContent: "center",
 		alignItems: "center",
-		width: "100%",
-		height: "27.5%",
+		height: heightPercentage(44),
 		backgroundColor: "white",
 	},
 	titleStyle: {
-		marginTop: "7%",
+		marginTop: heightPercentage(25),
 		fontFamily: "KoreanYNSJG3",
-		fontSize: RFValue(15),
+		fontSize: fontPercentage(15),
 		letterSpacing: 1,
 		fontStyle: "normal",
 		textAlign: "center",
 		color: "#000000",
 	},
 	subtitleStyle: {
-		marginTop: "3%",
+		marginTop: heightPercentage(15),
 		fontFamily: "KoreanYNSJG3",
-		fontSize: RFValue(10),
+		fontSize: fontPercentage(10),
 		fontStyle: "normal",
 		textAlign: "center",
 		color: "gray",
 	},
 	firstBtnStyle: {
 		flex: 1,
+		height: heightPercentage(44),
 		justifyContent: "center",
 		alignItems: "center",
-		width: "100%",
-		height: "100%",
 		backgroundColor: color.mainColor,
 	},
 	secondBtnStyle: {
 		flex: 1,
+		height: heightPercentage(44),
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "white",
 	},
 	firstBtnTitleStyle: {
 		fontFamily: "KoreanYNSJG3",
-		fontSize: RFValue(15),
+		fontSize: fontPercentage(15),
 		fontStyle: "normal",
 		textAlign: "center",
 		color: "#ffffff",
 	},
 	secondBtnTitleStyle: {
 		fontFamily: "KoreanYNSJG3",
-		fontSize: RFValue(15),
+		fontSize: fontPercentage(15),
 		fontStyle: "normal",
 		textAlign: "center",
 		color: "#000000",
