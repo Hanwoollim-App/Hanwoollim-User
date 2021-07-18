979f8326ec2cb8eb4710f4dd309741dc74d3342a
Kim Dong Hyun <akainoo123@gmail.com>
2021-07-18 16:41:32 +0900

    feat: publish Home Component

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { StyleSheet, BackHandler, Text, View } from 'react-native';
+import { BackHandler, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAndroidBackHandler } from 'react-navigation-backhandler';
 import defaultStyle from '../../../utils/constant/common/design/DefaultStyle';
 import CustomHeader from '../../common/CustomHeader';
 import CustomStatusBar from '../../common/CustomStatusBar';
-
-const styles = StyleSheet.create({
-	root: {
-		flex: 1,
-		width: '100%',
-	},
-});
 
 function Home() {
 	useAndroidBackHandler(() => {
@@ -22,11 +15,13 @@ function Home() {
 	return (
 		<>
 			<CustomStatusBar />
-			<SafeAreaView style={styles.root}>
+			<SafeAreaView style={defaultStyle.root} edges={['bottom']}>
 				<View style={defaultStyle.header}>
 					<CustomHeader />
 				</View>
-				<Text>홈화면입니다.</Text>
+				<View style={defaultStyle.contents}>
+					<Text>홈화면입니다.</Text>
+				</View>
 			</SafeAreaView>
 		</>
 	);
--- app/utils/constant/common/design/DefaultStyle.ts
+++ app/utils/constant/common/design/DefaultStyle.ts
@@ -1,8 +1,19 @@
 import { StyleSheet } from 'react-native';
 
 const defaultStyle = StyleSheet.create({
+	root: {
+		flex: 1,
+		width: '100%',
+		alignItems: 'center',
+	},
 	header: {
-		flexBasis: '7%',
+		flex: 1,
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	contents: {
+		flex: 13.25,
 		width: '100%',
 		justifyContent: 'center',
 		alignItems: 'center',
