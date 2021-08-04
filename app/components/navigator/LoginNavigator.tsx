e397e2a74389ac899e0be003e4597b7bc11306a7
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-08-04 11:26:26 +0900

    fix: 안드로이드 빌드 오류 해결
    앱 아이콘, 폰트까지 초기화 됨

--- app/components/navigator/LoginNavigator.tsx
+++ app/components/navigator/LoginNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/login';
-import SignUp from '../screens/signUp';
+import SignUp from '../screens/SignUp';
 
 const LoginStack = createStackNavigator();
 
