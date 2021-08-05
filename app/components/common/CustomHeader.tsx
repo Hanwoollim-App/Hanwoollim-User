8926b144c76a98a54444893946ca389a9c4ea75c
Aperture507 <williamzz@naver.com>
2021-08-05 15:40:20 +0900

    feat: screenWrapper에 뒤로가기 버튼 추가

--- app/components/common/CustomHeader.tsx
+++ app/components/common/CustomHeader.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
 import color from '../../utils/constant/common/design/Color';
-import { fontPercentage } from '../../utils/constant/common/design/Responsive';
-
-interface customHeader {
-	title?: string;
-}
+import {
+	fontPercentage,
+	heightPercentage,
+	widthPercentage,
+} from '../../utils/constant/common/design/Responsive';
 
 const styles = StyleSheet.create({
 	root: {
 		height: '100%',
 		width: '100%',
+		flexDirection: 'row',
 		backgroundColor: color.mainColor,
 		justifyContent: 'center',
 		alignItems: 'center',
@@ -26,21 +27,91 @@ const styles = StyleSheet.create({
 		color: '#ffffff',
 	},
 	logoImg: {
-		width: '55%',
+		width: '70%',
 		height: '80%',
 		resizeMode: 'cover',
 	},
+	header: {
+		width: '100%',
+		height: heightPercentage(57),
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: color.mainColor,
+	},
+	headerText: {
+		fontFamily: 'NotoSansKR-Bold',
+		fontSize: fontPercentage(20),
+		color: 'white',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	headerCenter: {
+		flex: 76,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	headerLeft: {
+		flex: 12,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	headerRight: {
+		flex: 12,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
+interface HeaderInterface {
+	title?: string;
+	headerLeft?: boolean;
+	headerRight?: boolean;
+	leftIcon?: React.ReactNode;
+	rightIcon?: React.ReactNode;
+	leftIconClickListener?: Function;
+	rightIconClickListener?: Function;
+}
 
 const logo = require('../../assets/images/textLogo_light.png');
 
-function CustomHeader({ title }: customHeader) {
+function CustomHeader({
+	title,
+	headerLeft = false,
+	headerRight = false,
+	leftIcon,
+	rightIcon,
+	leftIconClickListener = () => {},
+	rightIconClickListener = () => {},
+}: HeaderInterface) {
 	return (
 		<View style={styles.root}>
-			{title ? (
-				<Text style={styles.title}>{title}</Text>
+			{headerLeft ? (
+				<TouchableOpacity
+					onPress={() => leftIconClickListener()}
+					style={styles.headerLeft}>
+					{leftIcon}
+				</TouchableOpacity>
 			) : (
-				<Image source={logo} style={styles.logoImg} />
+				<View style={styles.headerLeft}></View>
+			)}
+			{title ? (
+				<View style={styles.headerCenter}>
+					<Text style={styles.title}>{title}</Text>
+				</View>
+			) : (
+				<View style={styles.headerCenter}>
+					<Image source={logo} style={styles.logoImg} />
+				</View>
+			)}
+			{headerRight ? (
+				<TouchableOpacity
+					onPress={() => leftIconClickListener()}
+					style={styles.headerRight}>
+					{rightIcon}
+				</TouchableOpacity>
+			) : (
+				<View style={styles.headerRight}></View>
 			)}
 		</View>
 	);
--- app/components/common/ScreenWrapper.tsx
+++ app/components/common/ScreenWrapper.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import { View } from 'react-native';
+import {
+	NavigationProp,
+	ParamListBase,
+	useNavigation,
+} from '@react-navigation/native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import defaultStyle from '../../utils/constant/common/design/DefaultStyle';
-import CustomHeader from './CustomHeader';
+import HeaderInterface from './CustomHeader';
 import CustomStatusBar from './CustomStatusBar';
 
 interface screenWrapper {
 	children: React.ReactNode;
 	headerTitle?: string;
 }
+const icon = (
+	<FontAwesomeIcon style={{ color: 'white' }} icon={faChevronLeft} />
+);
 
 function ScreenWrapper({ children, headerTitle }: screenWrapper) {
+	const navigation: NavigationProp<ParamListBase> = useNavigation();
+
 	return (
 		<>
 			<CustomStatusBar />
 			<SafeAreaView style={defaultStyle.root} edges={['bottom']}>
 				<View style={defaultStyle.header}>
-					<CustomHeader title={headerTitle ?? undefined} />
+					<HeaderInterface
+						title={headerTitle ?? undefined}
+						headerLeft
+						leftIcon={icon}
+						leftIconClickListener={navigation.goBack}
+					/>
 				</View>
 				<View style={defaultStyle.contents}>{children}</View>
 			</SafeAreaView>
