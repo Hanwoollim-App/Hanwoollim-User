7e13fa719c7cfda417b1548d2e2613a14dd50fcc
Aperture507 <williamzz@naver.com>
2021-08-11 13:49:27 +0900

    feat: 자료 게시판 퍼블리싱

--- app/components/screens/board/index.tsx
+++ app/components/screens/board/index.tsx
@@ -1,11 +1,67 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import {
+	fontPercentage,
+	heightPercentage,
+	widthPercentage,
+} from '../../../utils/constant/common/design/Responsive';
+
+import ScreenWrapper from '../../common/ScreenWrapper';
+
+const styles = StyleSheet.create({
+	block: {
+		height: heightPercentage(83),
+		width: widthPercentage(335),
+		borderRadius: widthPercentage(10),
+		backgroundColor: '#ffffff',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	blockTitle: {
+		fontFamily: 'NotoSansKR-Bold',
+		fontSize: fontPercentage(20),
+		lineHeight: fontPercentage(25),
+		fontWeight: '300',
+		fontStyle: 'normal',
+		letterSpacing: 0,
+		textAlign: 'center',
+		color: '#000000',
+	},
+	blockContent: {
+		fontFamily: 'NotoSansKR-Regular',
+		fontSize: fontPercentage(12),
+		lineHeight: fontPercentage(16),
+		fontWeight: 'normal',
+		fontStyle: 'normal',
+		letterSpacing: 0,
+		textAlign: 'center',
+		color: '#000000',
+	},
+	board: {
+		width: '100%',
+		height: '40%',
+		marginTop: '45%',
+	},
+	boardImg: {
+		width: '100%',
+		height: '100%',
+		resizeMode: 'contain',
+	},
+});
+
+const board = require('../../../assets/images/board.png');
 
 function Board() {
 	return (
-		<View>
-			<Text>{'게시판 화면입니다.'}</Text>
-		</View>
+		<ScreenWrapper headerTitle="자료게시판">
+			<View style={styles.board}>
+				<Image style={styles.boardImg} source={board} />
+			</View>
+			<View style={styles.block}>
+				<Text style={styles.blockTitle}>기능 준비중입니다</Text>
+				<Text style={styles.blockContent}>조금만 기다려줘요... 약속?</Text>
+			</View>
+		</ScreenWrapper>
 	);
 }
 
